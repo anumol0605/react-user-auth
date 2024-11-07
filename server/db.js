cdb465365e1272f72e23af93ded9945136fe60cd
ngit90 <88375030+ngit90@users.noreply.github.com>
2024-11-07 15:52:17 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ module.exports = () => {
 		useUnifiedTopology: true,
 	};*/
 	try {
-		mongoose.connect("mongodb://localhost:27017/mywebapp");
+		mongoose.connect(process.env.DB_URL);
 		console.log("Connected to database successfully");
 	} catch (error) {
 		console.log(error);
